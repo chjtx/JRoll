44dfd0be2ed56ede46c654f5cf62008eff1233ca
BarZu <1419328765@qq.com>
2016-11-29 16:56:12 +0800

    eslint规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    "extends": "standard"
 };
