bf149911da4f4fe737310c7ba713c9e6994bc9e3
BarZu <1419328765@qq.com>
2017-05-03 14:06:13 +0800

    JRoll-Infinite v2.1.4

--- extends/jroll-infinite/build/jroll-infinite.2.1.4.js
+++ extends/jroll-infinite/build/jroll-infinite.2.1.4.js
@@ -1,3 +1,4 @@
+/*! JRoll-Infinite v2.1.4 ~ (c) 2016 Author:BarZu Git:https://github.com/chjtx/JRoll/tree/master/extends/jroll-infinite */
 /* global define, JRoll */
 (function (window, document, JRoll) {
   'use strict'
@@ -187,7 +188,7 @@
     }
   }
 
-  JRoll.prototype.infinite.version = '{{version}}'
+  JRoll.prototype.infinite.version = '2.1.4'
 
   // CommonJS/AMD/CMD规范导出JRoll
   if (typeof module !== 'undefined' && module.exports) {
